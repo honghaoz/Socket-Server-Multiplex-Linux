116b5c3b3fa8c75dac87865fc3e4bbfe43dce52f
Honghao Zhang <zhh358@gmail.com>
2014-06-17 21:31:10 -0400

    Initial Commit

---
+++ server_multiple/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  server_multiple
+//
+//  Created by Zhang Honghao on 6/17/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
